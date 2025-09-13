a777fd0aad05484a3d87f6e70cdadb2d5878b6eb
Rodrigo <rodrigo.e.sanchez.araya@gmail.com>
2025-09-12 18:10:37 -0600

    Se corrige, prueba 2 estaba repetida
    Signed-off-by: Rodrigo <rodrigo.e.sanchez.araya@gmail.com>

--- Tarea_1/src/probador.v
+++ Tarea_1/src/probador.v
@@ -63,40 +63,6 @@ initial begin
     //reset 
     #50 reset = 0; 
     #5 reset = 1; 
-    // PRUEBA 2
-    
-    // Inserta tarjeta
-    #10 TARJETA_RECIBIDA = 1;
-    #10 TARJETA_RECIBIDA = 0;
-
-    // Ingreso de PIN: 7
-    #10 DIGITO = 4'b0111; DIGITO_STB = 1;
-    #10 DIGITO_STB = 0;
-
-    // Ingreso de PIN: 2
-    #10 DIGITO = 4'b0010; DIGITO_STB = 1;
-    #10 DIGITO_STB = 0;
-
-    // Ingreso de PIN: 5
-    #10 DIGITO = 4'b0101; DIGITO_STB = 1;
-    #10 DIGITO_STB = 0;
-
-    // Ingreso de PIN: 9
-    #10 DIGITO = 4'b1001; DIGITO_STB = 1;
-    #10 DIGITO_STB = 0;
-
-   
-    #5 TIPO_TRANS = 1;
-
-    // Ingresa monto
-    #10 MONTO = 32'd50000;
-    #5 MONTO_STB = 1;
-    #10 MONTO_STB = 0;
-
-
-    //reset 
-    #50 reset = 0; 
-    #5 reset = 1; 
 
 // -------------------------------------------------------
     // PRUEBA 2
@@ -240,7 +206,7 @@ initial begin
 
 
 
-    
+
     #100 $finish;
 end
 
