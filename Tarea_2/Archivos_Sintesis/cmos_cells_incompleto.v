009f51c4b8976f5c22837e7c109ff236f1a31398
Rodrigo <rodrigo.e.sanchez.araya@gmail.com>
2025-09-23 16:08:37 -0600

    Se agregan archivos de sintesis a usar en la tarea
    Signed-off-by: Rodrigo <rodrigo.e.sanchez.araya@gmail.com>

---
+++ Tarea_2/Archivos_Sintesis/cmos_cells_incompleto.v
@@ -0,0 +1,23 @@
+module BUF(A, Y);
+  input A;
+  output Y;
+  assign Y = A;
+endmodule
+
+module NOT(A, Y);
+  input A;
+  output Y;
+  assign Y = ~A;
+endmodule
+
+module NAND(A, B, Y);
+  input A, B;
+  output Y;
+  assign Y = ~(A & B);
+endmodule
+
+module NOR(A, B, Y);
+  input A, B;
+  output Y;
+  assign Y = ~(A | B);
+endmodule
